257881c9de9ce8c54795a497960eee0e32a0e175
aymen2200 <aymen.djelida@gmail.com>
2025-12-02 15:23:12 +0100

    First commit in this project

---
+++ modules.js
@@ -0,0 +1,5 @@
+const name = require("./names-and-functions").name;
+const greet = require("./names-and-functions").greet;
+
+require("./mind-grenade");
+greet(name);
---
+++ names-and-functions.js
@@ -0,0 +1,5 @@
+const name = "ahmed";
+function greet() {
+  console.log(`hello ${name}` );
+}
+module.exports = {name,greet};
