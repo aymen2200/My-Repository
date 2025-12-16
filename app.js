57786868b4d4df3ee9fef6dd542455fc1236c5da
aymen2200 <aymen.djelida@gmail.com>
2025-12-16 22:04:40 +0100

    event loop and async patterns

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
-const _=require('lodash')
-const items = [1,[2,[3,[4]]]]
-const flattenItems = _.flattenDeep(items);
-console.log(flattenItems)
-console.log('global install')
+const { readFile, writeFile } = require("fs").promises;
+
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf8");
+    const second = await readFile("./content/second.txt", "utf8");
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `this is awesome : ${first} ${second}`
+    );
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
+
+// getText('./content/first.txt')
+// .then((result) => console.log(result))
+// .catch((err) => console.log(err))
