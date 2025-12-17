cee911d5828b59318fbdc93f5ab680452308b503
aymen2200 <aymen.djelida@gmail.com>
2025-12-17 11:25:09 +0100

    the same as precvious

--- modules.js
+++
@@ -1,5 +0,0 @@
-const name = require("./names-and-functions").name;
-const greet = require("./names-and-functions").greet;
-
-require("./mind-grenade");
-greet(name);
--- names-and-functions.js
+++
@@ -1,5 +0,0 @@
-const name = "ahmed";
-function greet() {
-  console.log(`hello ${name}` );
-}
-module.exports = {name,greet};
